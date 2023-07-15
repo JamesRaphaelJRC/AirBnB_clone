99c99d6d6e2390b766ab48fd4b8649f48003ccdd
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-07-15 13:05:54 -0700

    master

--- models/city.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    '''defines City class'''
-    state_id = ''
-    name = ''
--- models/state.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    '''defines State Class '''
-    name = ''
